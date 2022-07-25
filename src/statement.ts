0567202b10479bc9c16fc1af7d759dfed5f82e72
eleven <sydryy@gmail.com>
2022-07-25 09:14:37 -0700

    refactor step 1

--- src/statement.ts
+++ src/statement.ts
@@ -2,40 +2,57 @@ export function statement (invoice: any, plays: any) {
 	let totalAmount = 0
 	let volumeCredits = 0
 	let result = `Statement for ${invoice.customer}\n`
-	const format = new Intl.NumberFormat('en-US',
-		{
-			style: 'currency', currency: 'USD',
-			minimumFractionDigits: 2
-		}).format
-	for (let perf of invoice.performances) {
-		const play = plays[perf.playID]
-		let thisAmount = 0
-		switch (play.type) {
+
+	function numberToCurrency (aNumber: number) {
+		return new Intl.NumberFormat('en-US',
+			{
+				style: 'currency',
+				currency: 'USD',
+				minimumFractionDigits: 2
+			}).format(aNumber / 100)
+	}
+
+	function getPlay (performance: any) {
+		return plays[performance.playID]
+	}
+
+	function audienceBonus (aPerformance: any) {
+		let result = 0
+		switch (getPlay(aPerformance).type) {
 			case 'tragedy':
-				thisAmount = 40000
-				if (perf.audience > 30) {
-					thisAmount += 1000 * (perf.audience - 30)
+				result = 40000
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30)
 				}
 				break
 			case 'comedy':
-				thisAmount = 30000
-				if (perf.audience > 20) {
-					thisAmount += 10000 + 500 * (perf.audience - 20)
+				result = 30000
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20)
 				}
-				thisAmount += 300 * perf.audience
+				result += 300 * aPerformance.audience
 				break
 			default:
-				throw new Error(`unknown type: ${play.type}`)
+				throw new Error(`unknown type: ${getPlay(aPerformance).type}`)
 		}
-// add volume credits
-		volumeCredits += Math.max(perf.audience - 30, 0)
-// add extra credit for every ten comedy attendees
-		if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5)
-// print line for this order
-		result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`
-		totalAmount += thisAmount
+		return result
 	}
-	result += `Amount owed is ${format(totalAmount / 100)}\n`
+
+	function calculateVolumeCredits (aPerformance: any) {
+		let result = 0
+		result += Math.max(aPerformance.audience - 30, 0)
+		if ('comedy' === getPlay(aPerformance).type) result += Math.floor(aPerformance.audience / 5)
+		return result
+	}
+
+	for (let perf of invoice.performances) {
+		volumeCredits += calculateVolumeCredits(perf)
+
+		// print line for this order
+		result += ` ${getPlay(perf).name}: ${numberToCurrency((audienceBonus(perf)))} (${(perf.audience)} seats)\n`
+		totalAmount += audienceBonus(perf)
+	}
+	result += `Amount owed is ${numberToCurrency((totalAmount))}\n`
 	result += `You earned ${volumeCredits} credits\n`
 	return result
 }
