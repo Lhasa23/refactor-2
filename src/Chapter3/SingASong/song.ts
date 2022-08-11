cb29a7965c0944ee5af53ab811207276f46a7996
eleven <sydryy@gmail.com>
2022-08-11 01:41:12 -0700

    refactor - split verse

--- src/Chapter3/SingASong/song.ts
+++ src/Chapter3/SingASong/song.ts
@@ -1,43 +1,75 @@
-const swallowedFly = `I don't know why she swallowed a fly - perhaps she'll die!`
+const curseHerDie = `I don't know why she swallowed a fly - perhaps she'll die!\n`
 
 function readyToSwallow (animal: string) {
 	return `There was an old lady who swallowed a ${animal}`
 }
 
-const song = `There was an old lady who swallowed a fly.
-${swallowedFly}
-There was an old lady who swallowed a spider;
+function swallowedToCatch (swallowed: string, toCatch: string) {
+	return `She swallowed the ${swallowed} to catch the ${toCatch}`
+}
+
+function swallowAddSpider () {
+	return `There was an old lady who swallowed a spider;
 That wriggled and wiggled and tickled inside her.
-She swallowed the spider to catch the fly;
-${swallowedFly}
-${readyToSwallow('bird')};
+${swallowedToCatch('spider', 'fly')};
+${curseHerDie}`
+}
+
+function startToSwallow () {
+	return `There was an old lady who swallowed a fly.
+${curseHerDie}`
+}
+
+function swallowAddBird () {
+	return `${readyToSwallow('bird')};
 How absurd to swallow a bird.
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-${swallowedFly}
-${readyToSwallow('cat')};
+${swallowedToCatch('bird', 'spider')},
+${swallowedToCatch('spider', 'fly')};
+${curseHerDie}`
+}
+
+function swallowAddCat () {
+	return `${readyToSwallow('cat')};
 Fancy that to swallow a cat!
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-${swallowedFly}
-${readyToSwallow('dog')};
+${swallowedToCatch('cat', 'bird')},
+${swallowedToCatch('bird', 'spider')},
+${swallowedToCatch('spider', 'fly')};
+${curseHerDie}`
+}
+
+function swallowAddDog () {
+	return `${readyToSwallow('dog')};
 What a hog, to swallow a dog!
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-${swallowedFly}
-${readyToSwallow('cow')};
+${swallowedToCatch('dog', 'cat')},
+${swallowedToCatch('cat', 'bird')},
+${swallowedToCatch('bird', 'spider')},
+${swallowedToCatch('spider', 'fly')};
+${curseHerDie}`
+}
+
+function swallowAddCow () {
+	return `${readyToSwallow('cow')};
 I don't know how she swallowed a cow!
-She swallowed the cow to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-${swallowedFly}
-${readyToSwallow('horse')}...
+${swallowedToCatch('cow', 'dog')},
+${swallowedToCatch('dog', 'cat')},
+${swallowedToCatch('cat', 'bird')},
+${swallowedToCatch('bird', 'spider')},
+${swallowedToCatch('spider', 'fly')};
+${curseHerDie}`
+}
+
+function endToSwallow () {
+	return `${readyToSwallow('horse')}...
 ...She's dead, of course!`
+}
+
+const song = startToSwallow() +
+	swallowAddSpider() +
+	swallowAddBird() +
+	swallowAddCat() +
+	swallowAddDog() +
+	swallowAddCow() +
+	endToSwallow()
 
 export default function sing () {
 	return song
