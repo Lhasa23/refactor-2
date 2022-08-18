1be0b2852af270b60ab5a31767c04d186b6190db
eleven <sydryy@gmail.com>
2022-08-17 21:29:42 -0700

    refactor Repeated Switches(if/else form)

--- src/Chapter3/GildedRose/gilded-rose.ts
+++ src/Chapter3/GildedRose/gilded-rose.ts
@@ -20,47 +20,48 @@ export class GildedRose {
 	updateQuality () {
 		for (let i = 0; i < this.items.length; i++) {
 			const currentItem = this.items[i]
-			if (currentItem.name === 'Sulfuras, Hand of Ragnaros') continue
 
-			if (currentItem.name === 'Backstage passes to a TAFKAL80ETC concert') {
-				currentItem.quality = currentItem.quality + 1
-
-				if (currentItem.sellIn < 11) {
+			switch (currentItem.name) {
+				case 'Sulfuras, Hand of Ragnaros':
+					break
+				case 'Backstage passes to a TAFKAL80ETC concert':
 					currentItem.quality = currentItem.quality + 1
-				}
-				if (currentItem.sellIn < 6) {
+
+					if (currentItem.sellIn < 11) {
+						currentItem.quality = currentItem.quality + 1
+					}
+					if (currentItem.sellIn < 6) {
+						currentItem.quality = currentItem.quality + 1
+					}
+					if (currentItem.quality >= 50) {
+						currentItem.quality = 50
+					}
+
+					if (--currentItem.sellIn >= 0) break
+					currentItem.quality = 0
+					break
+				case 'Aged Brie':
 					currentItem.quality = currentItem.quality + 1
-				}
-				if (currentItem.quality >= 50) {
-					currentItem.quality = 50
-				}
+					if (currentItem.quality >= 50) {
+						currentItem.quality = 50
+					}
+					currentItem.sellIn = currentItem.sellIn - 1
+					if (currentItem.sellIn >= 0) break
+					if (currentItem.quality >= 50) break
+					currentItem.quality = currentItem.quality + 1
+					break
+				default:
+					if (currentItem.quality > 0) {
+						currentItem.quality = currentItem.quality - 1
+					}
 
-				if (--currentItem.sellIn >= 0) continue
-				currentItem.quality = 0
-				continue
-			}
+					currentItem.sellIn = currentItem.sellIn - 1
+					if (currentItem.sellIn >= 0) break
 
-			if (currentItem.name === 'Aged Brie') {
-				currentItem.quality = currentItem.quality + 1
-				if (currentItem.quality >= 50) {
-					currentItem.quality = 50
-				}
-				currentItem.sellIn = currentItem.sellIn - 1
-				if (currentItem.sellIn >= 0) continue
-				if (currentItem.quality >= 50) continue
-				currentItem.quality = currentItem.quality + 1
-				continue
-			}
-
-			if (currentItem.quality > 0) {
-				currentItem.quality = currentItem.quality - 1
-			}
-
-			currentItem.sellIn = currentItem.sellIn - 1
-			if (currentItem.sellIn >= 0) continue
-
-			if (currentItem.quality > 0) {
-				currentItem.quality = currentItem.quality - 1
+					if (currentItem.quality > 0) {
+						currentItem.quality = currentItem.quality - 1
+					}
+					break
 			}
 		}
 
