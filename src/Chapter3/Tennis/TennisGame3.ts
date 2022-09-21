101b65cba8fdbaebe6fe7be676ba7a149677ced6
eleven <sydryy@gmail.com>
2022-09-20 23:49:42 -0700

    complete game 3

--- src/Chapter3/Tennis/TennisGame3.ts
+++ src/Chapter3/Tennis/TennisGame3.ts
@@ -1,34 +1,37 @@
-import { TennisGame } from './TennisGame';
+import { TennisGame } from './TennisGame'
 
 export class TennisGame3 implements TennisGame {
-  private p2: number = 0;
-  private p1: number = 0;
-  private p1N: string;
-  private p2N: string;
+	private p2: number = 0
+	private p1: number = 0
+	private readonly p1Name: string
+	private readonly p2Name: string
 
-  constructor(p1N: string, p2N: string) {
-    this.p1N = p1N;
-    this.p2N = p2N;
-  }
+	constructor (p1N: string, p2N: string) {
+		this.p1Name = p1N
+		this.p2Name = p2N
+	}
 
-  getScore(): string {
-    let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
-      const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
-    } else {
-      if (this.p1 === this.p2)
-        return 'Deuce';
-      s = this.p1 > this.p2 ? this.p1N : this.p2N;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
-    }
-  }
+	getScore (): string {
+		const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty']
+		if (this.p1 === this.p2)
+			return this.p1 < 3 ? p[this.p1] + '-All' : 'Deuce'
 
-  wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.p1 += 1;
-    else
-      this.p2 += 1;
-  }
+		if (this.p1 < 4 && this.p2 < 4) {
+			return p[this.p1] + '-' + p[this.p2]
+		}
+
+		return this.differ ? 'Advantage ' + this.winner : 'Win for ' + this.winner
+	}
+
+	wonPoint (playerName: string): void {
+		playerName === 'player1' ? this.p1++ : this.p2++
+	}
+
+	private get differ () {
+		return ((this.p1 - this.p2) * (this.p1 - this.p2)) === 1
+	}
+
+	private get winner () {
+		return this.p1 > this.p2 ? this.p1Name : this.p2Name
+	}
 }
